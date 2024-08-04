54f28fbede9437cbaae9845c6d895d2d43a69632
NagelDylan <dylan.nagel2022@gmail.com>
2024-08-04 01:14:47 -0400

    pushed new design

--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import * as pdfjsLib from 'pdfjs-dist';
-import api from '../api';
-import '../styles/Home.css';
-import styled from 'styled-components';
+import React, { useRef, useState, useEffect } from "react";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import * as pdfjsLib from "pdfjs-dist";
+import api from "../api";
+import "../styles/Home.css";
+import styled from "styled-components";
+import delIcon from "/delete.svg";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`;
 
@@ -21,7 +22,7 @@ class ErrorBoundary extends React.Component {
   }
 
   componentDidCatch(error, errorInfo) {
-    console.error('Error caught in ErrorBoundary:', error, errorInfo);
+    console.error("Error caught in ErrorBoundary:", error, errorInfo);
   }
 
   render() {
@@ -36,14 +37,14 @@ class ErrorBoundary extends React.Component {
 const PDFViewer = () => {
   const viewerRef = useRef(null);
   const [highlightedSpan, setHighlightedSpan] = useState(null);
-  const [note, setNote] = useState('');
+  const [note, setNote] = useState("");
   const [notes, setNotes] = useState([]);
-  const [title, setTitle] = useState('');
-  const [gptResponse, setGptResponse] = useState('');
+  const [title, setTitle] = useState("");
+  const [gptResponse, setGptResponse] = useState("");
   const [pdfFile, setPdfFile] = useState(null);
   const [uploadedPapers, setUploadedPapers] = useState([]);
   const [selectedPaperId, setSelectedPaperId] = useState(null);
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
@@ -51,34 +52,36 @@ const PDFViewer = () => {
     const handleTextDoubleClick = async (event) => {
       const target = event.target;
 
-      if (target && target.classList.contains('rpv-core__text-layer-text')) {
+      if (target && target.classList.contains("rpv-core__text-layer-text")) {
         if (highlightedSpan) {
-          highlightedSpan.classList.remove('highlight');
+          highlightedSpan.classList.remove("highlight");
         }
 
-        target.classList.add('highlight');
+        target.classList.add("highlight");
         setHighlightedSpan(target);
 
         const currentText = target.textContent;
 
-        console.log('Highlighted text:', currentText);
+        console.log("Highlighted text:", currentText);
 
         try {
-          const response = await api.post('/api/explain/', { current_text: currentText });
+          const response = await api.post("/api/explain/", {
+            current_text: currentText,
+          });
           setGptResponse(response.data.explanation);
         } catch (err) {
-          console.error('Error during API request:', err);
-          alert('Failed to get explanation.');
+          console.error("Error during API request:", err);
+          alert("Failed to get explanation.");
         }
       }
     };
 
     const container = viewerRef.current;
     if (container) {
-      container.addEventListener('dblclick', handleTextDoubleClick);
+      container.addEventListener("dblclick", handleTextDoubleClick);
 
       return () => {
-        container.removeEventListener('dblclick', handleTextDoubleClick);
+        container.removeEventListener("dblclick", handleTextDoubleClick);
       };
     }
   }, [highlightedSpan]);
@@ -95,88 +98,95 @@ const PDFViewer = () => {
   }, [selectedPaperId]);
 
   const getNotes = () => {
-    api.get('/api/notes/')
+    api
+      .get("/api/notes/")
       .then((res) => setNotes(res.data))
-      .catch((err) => alert('Failed to fetch notes.'));
+      .catch((err) => alert("Failed to fetch notes."));
   };
 
   const fetchUploadedPapers = () => {
-    api.get('/api/papers/')
+    api
+      .get("/api/papers/")
       .then((res) => {
-        console.log('Fetched papers:', res.data);
+        console.log("Fetched papers:", res.data);
         setUploadedPapers(res.data);
       })
-      .catch((err) => alert('Failed to fetch papers.'));
+      .catch((err) => alert("Failed to fetch papers."));
   };
 
   const fetchComments = (paperId) => {
-    api.get(`/api/papers/${paperId}/comments/`)
+    api
+      .get(`/api/papers/${paperId}/comments/`)
       .then((res) => {
-        console.log('Fetched comments:', res.data);
+        console.log("Fetched comments:", res.data);
         setComments(res.data);
       })
       .catch((err) => {
-        console.error('Failed to fetch comments:', err);
-        alert('Failed to fetch comments.');
+        console.error("Failed to fetch comments:", err);
+        alert("Failed to fetch comments.");
       });
   };
 
   const createNote = (e) => {
     e.preventDefault();
-    api.post('/api/notes/', { content: note, title })
+    api
+      .post("/api/notes/", { content: note, title })
       .then((res) => {
         if (res.status === 201) {
           getNotes();
-          setNote('');
-          setTitle('');
+          setNote("");
+          setTitle("");
         } else {
-          alert('Failed to create note.');
+          alert("Failed to create note.");
         }
       })
-      .catch((err) => alert('Failed to create note.'));
+      .catch((err) => alert("Failed to create note."));
   };
 
   const deleteNote = (id) => {
-    api.delete(`/api/notes/delete/${id}/`)
+    api
+      .delete(`/api/notes/delete/${id}/`)
       .then((res) => {
         if (res.status === 204) {
           getNotes();
         } else {
-          alert('Failed to delete note.');
+          alert("Failed to delete note.");
         }
       })
-      .catch((err) => alert('Failed to delete note.'));
+      .catch((err) => alert("Failed to delete note."));
   };
 
   const createComment = (e) => {
     e.preventDefault();
-    api.post(`/api/papers/${selectedPaperId}/comments/`, { content: comment })
+    api
+      .post(`/api/papers/${selectedPaperId}/comments/`, { content: comment })
       .then((res) => {
         if (res.status === 201) {
-          alert('Comment created!');
+          alert("Comment created!");
           fetchComments(selectedPaperId);
-          setComment('');
+          setComment("");
         } else {
-          alert('Failed to create comment.');
+          alert("Failed to create comment.");
         }
       })
       .catch((err) => {
-        console.error('Failed to create comment:', err);
-        alert('Failed to create comment.');
+        console.error("Failed to create comment:", err);
+        alert("Failed to create comment.");
       });
   };
 
   const deleteComment = (id) => {
-    api.delete(`/api/comments/delete/${id}/`)
+    api
+      .delete(`/api/comments/delete/${id}/`)
       .then((res) => {
         if (res.status === 204) {
-          alert('Comment deleted!');
+          alert("Comment deleted!");
           fetchComments(selectedPaperId);
         } else {
-          alert('Failed to delete comment.');
+          alert("Failed to delete comment.");
         }
       })
-      .catch((err) => alert('Failed to delete comment.'));
+      .catch((err) => alert("Failed to delete comment."));
   };
 
   const handleNoteChange = (event) => {
@@ -189,16 +199,17 @@ const PDFViewer = () => {
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    if (file && file.type === 'application/pdf') {
+    if (file && file.type === "application/pdf") {
       const formData = new FormData();
-      formData.append('file', file);
-      formData.append('title', file.name);
+      formData.append("file", file);
+      formData.append("title", file.name);
 
-      api.post('/api/upload/', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
+      api
+        .post("/api/upload/", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
         .then((res) => {
           if (res.status === 201) {
             fetchUploadedPapers();
@@ -207,46 +218,55 @@ const PDFViewer = () => {
             setPdfFile(URL.createObjectURL(file));
             fetchComments(newPaperId);
           } else {
-            alert('Failed to upload PDF.');
+            alert("Failed to upload PDF.");
           }
         })
         .catch((err) => {
-          console.error('Upload error:', err);
-          alert('Failed to upload PDF.');
+          console.error("Upload error:", err);
+          alert("Failed to upload PDF.");
         });
     } else {
-      alert('Please upload a valid PDF file.');
+      alert("Please upload a valid PDF file.");
     }
   };
 
   const handlePaperClick = (paper) => {
     const filePath = `http://127.0.0.1:8000/api/media/your_upload_directory/${paper.title}`;
-    console.log('File path:', filePath);
+    console.log("File path:", filePath);
     setPdfFile(filePath);
     fetchComments(paper.id);
   };
 
   return (
     <ErrorBoundary>
-      <div style={{ height: '100vh', width: '100vw', display: 'flex' }}>
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-          <div style={{ display: 'flex', flexWrap: 'wrap', padding: '10px' }}>
-            {uploadedPapers.map((paper) => (
-              <div key={paper.id} style={{ margin: '5px', padding: '5px', border: '1px solid #ccc', cursor: 'pointer' }} onClick={() => handlePaperClick(paper)}>
-                {paper.title}
-              </div>
-            ))}
-          </div>
-          <div ref={viewerRef} style={{ flex: 1, overflow: 'auto', position: 'relative' }}>
+      <div style={{ height: "100vh", width: "100vw", display: "flex" }}>
+        <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+          <PdfButtonContainer>
             <input
               type="file"
               accept=".pdf"
               onChange={handleFileChange}
-              style={{ margin: '10px', display: 'block' }}
+              style={{ width: "80px" }}
             />
+            {uploadedPapers.map((paper, index) => (
+              <PdfButtons key={index} onClick={() => handlePaperClick(paper)}>
+                {paper.title}
+                <img src={delIcon} alt="delete icon" />
+              </PdfButtons>
+            ))}
+          </PdfButtonContainer>
+          <div
+            ref={viewerRef}
+            style={{ flex: 1, overflow: "auto", position: "relative" }}
+          >
             {pdfFile && (
-              <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`}>
-                <Viewer fileUrl={pdfFile} plugins={[defaultLayoutPluginInstance]} />
+              <Worker
+                workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`}
+              >
+                <Viewer
+                  fileUrl={pdfFile}
+                  plugins={[defaultLayoutPluginInstance]}
+                />
               </Worker>
             )}
           </div>
@@ -259,45 +279,64 @@ const PDFViewer = () => {
               placeholder="Note title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
-              style={{ width: '100%', marginBottom: '10px' }}
+              style={{ width: "100%", marginBottom: "10px" }}
             />
             <textarea
               placeholder="Note content"
               value={note}
               onChange={handleNoteChange}
-              style={{ width: '100%', height: '100px', marginBottom: '10px' }}
+              style={{ width: "100%", height: "100px", marginBottom: "10px" }}
             />
             <button type="submit">Add Note</button>
           </form>
           {notes.map((note) => (
-            <div key={note.id} style={{ marginBottom: '10px' }}>
+            <NoteContainer key={note.id}>
               <h3>{note.title}</h3>
               <p>{note.content}</p>
               <button onClick={() => deleteNote(note.id)}>Delete Note</button>
-            </div>
+            </NoteContainer>
           ))}
           {gptResponse && (
-            <div style={{ marginTop: '20px', padding: '10px', border: '1px solid #ccc' }}>
+            <div
+              style={{
+                marginTop: "20px",
+                padding: "10px",
+                border: "1px solid #ccc",
+              }}
+            >
               <h2>GPT Explanation</h2>
               <p>{gptResponse}</p>
             </div>
           )}
           {selectedPaperId && (
-            <div style={{ padding: '20px', maxHeight: '30vh', overflow: 'auto', border: '1px solid red' }}>
+            <div
+              style={{
+                padding: "20px",
+                maxHeight: "30vh",
+                overflow: "auto",
+                border: "1px solid red",
+              }}
+            >
               <h2>Comments</h2>
               <form onSubmit={createComment}>
                 <textarea
                   placeholder="Add a comment"
                   value={comment}
                   onChange={handleCommentChange}
-                  style={{ width: '100%', height: '100px', marginBottom: '10px' }}
+                  style={{
+                    width: "100%",
+                    height: "100px",
+                    marginBottom: "10px",
+                  }}
                 />
                 <button type="submit">Add Comment</button>
               </form>
               {comments.map((comment) => (
-                <div key={comment.id} style={{ marginBottom: '10px' }}>
+                <div key={comment.id} style={{ marginBottom: "10px" }}>
                   <p>{comment.content}</p>
-                  <button onClick={() => deleteComment(comment.id)}>Delete Comment</button>
+                  <button onClick={() => deleteComment(comment.id)}>
+                    Delete Comment
+                  </button>
                 </div>
               ))}
             </div>
@@ -319,23 +358,42 @@ const StickyColumn = styled.div`
   overflow: auto;
 `;
 
-// Styled component for the header
-const Header = styled.header`
+const PdfButtonContainer = styled.div`
   display: flex;
   align-items: center;
-  padding: 10px;
-  background-color: #f4f4f4;
-  border-bottom: 1px solid #ddd;
+  gap: 8px;
+  margin: 10px;
+`;
+
+const PdfButtons = styled.button`
+  background: #0e66ff;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  border: none;
+  font-size: 15px;
+  display: flex;
+
+  &:hover {
+    cursor: pointer;
+  }
 
   img {
-    height: 40px;
-    margin-right: 20px;
-  }
-
-  h1 {
-    font-size: 24px;
-    color: #333;
+    height: 16px;
+    margin-left: 8px;
   }
 `;
+
+const NoteContainer = styled.div `
+  background: #EEEEEE;
+  padding: 8px;
+  border-radius: 8px;
+
+  margin: 16px 0;
+
+  h3 {
+    margin-top: 0;
+  }
+`
 
 export default PDFViewer;
